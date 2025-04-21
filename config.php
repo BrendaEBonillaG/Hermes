eb435f5a5aceaa85ce0ee621f6df935cc1e89b11
BrendaEBonillaG <82917836+BrendaEBonillaG@users.noreply.github.com>
2025-04-21 11:31:56 -0600

    config

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 $host = 'localhost';
 $dbname = 'Hermes';
 $username = 'root';
-$password = '';
+$password = 'Admin';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
